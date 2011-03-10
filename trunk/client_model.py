3f68e935d8a2d7d6b0bbbe55597ff79b8867ab7c
moishel <moishel@33c5701d-68a6-59fb-37e6-850d17327217>
2011-03-10 03:19:43 +0000

    Zlib compress task queue data.

---
+++ trunk/client_model.py
@@ -0,0 +1,8 @@
+from google.appengine.ext import db
+
+class Client(db.Model):
+  """A record of a client connection. The string representation of the 'created'
+  field is the clientid used by the Channel API
+  """
+  created = db.DateTimeProperty(required=True, auto_now_add=True)
+    
